3607bad543cf89a941658a8ce4325318522575e6
Ramonlirani <ramonlirani@hotmail.com>
2021-06-04 16:36:27 -0300

    first commit

---
+++ JavaScript para iniciantes/atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,22 @@
+var numero = 20;
+var numero2= 10;
+
+numero /= numero2;
+console.log(numero)
+console.log(numero2)
+
+var idade = 19;
+
+var podeBeber;
+var naoPossuiDiabetes = true;
+
+podeBeber = (idade >= 18 && naoPossuiDiabetes) ? 'Pode beber': 'Não pode beber';
+// condição ? true : false
+
+console.log(podeBeber)
+
+var possuiFaculdade = true;
+if(possuiFaculdade)
+   console.log('Sim, possui');
+else
+  
