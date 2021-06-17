d4ce9eea3fc1eb5f0080cd833af3014371e09828
Ramonlirani <ramonlirani@hotmail.com>
2021-06-17 10:42:53 -0300

    native host e user

---
+++ objetos/native-host-e-user/script-aula.js
@@ -0,0 +1,8 @@
+console.log(typeof Array.fromdksopakdasop !== 'undefined')
+
+if(typeof Array.fromdksopakdasop !== 'undefined') {
+    console.log('Existe');
+}
+else{
+    console.log('Não Existe');
+}
